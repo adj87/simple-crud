0c2732fa2e9e0e94aec1dbe829628263e4307a13
Dr Jauregui <albertodomjauregui@hotmail.com>
2020-10-30 17:51:46 +0100

    solve linebreak eslint config problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>asdasd</p>
+
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React ; asd asd
         </a>
