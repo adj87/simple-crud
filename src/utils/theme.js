ddcbd559259cfe8f6c1d512fa87a931b160e2751
Dr Jauregui <albertodomjauregui@hotmail.com>
2020-10-30 18:01:05 +0100

    add first theme object

---
+++ src/utils/theme.js
@@ -0,0 +1,24 @@
+const theme = {
+  breakpoints: {
+    xs: 300,
+    sm: 500,
+    md: 700,
+    lg: 1021,
+  },
+  palette: {
+    primary: {
+      dark: '#03045E',
+      main: '#023E8A',
+      light: '#0077B6',
+      extraLight: '#48CAE4',
+      contrastText: '#CAF0F8',
+    },
+    secondary: {
+      dark: '#EE8F00',
+      main: '#FFC671',
+      light: '#FFDCA8',
+    },
+  },
+};
+
+export default theme;
