85961d74d331d9a5350a90317dcb70aae761d1db
Dr Jauregui <albertodomjauregui@hotmail.com>
2020-10-31 12:22:51 +0100

    more router config

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import ListView from './app/ListView/ListView';
 
 const routes = [
-  { path: '/', component: ListView },
   { path: '/login', component: () => <div>Login</div> },
+  { path: ['/', '/home'], component: ListView, exact: true },
+  { path: '*', component: () => <div>404</div> },
 ];
 
 export default routes;
