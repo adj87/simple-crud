622053babd58b1c385878e00bdc8699be0f45efe
Dr Jauregui <albertodomjauregui@hotmail.com>
2020-10-31 17:08:09 +0100

    implement button in login View

--- src/app/LoginView/index.jsx
+++ src/app/LoginView/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Modal from '../../components/Modal';
 import Header from '../../components/Header';
 import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const LoginView = () => (
   <>
@@ -9,6 +10,7 @@ const LoginView = () => (
     <Modal header="sign in">
       <Input label="User" value="yeah" />
       <Input label="Password" value="yeah" />
+      <Button onClick={() => console.log('ads')}>Sign in</Button>
     </Modal>
   </>
 );
