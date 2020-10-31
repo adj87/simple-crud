21304ed0d0bacf234674b27d203e141c624d18d2
Dr Jauregui <albertodomjauregui@hotmail.com>
2020-10-31 16:18:57 +0100

    add inputs to loginView

--- src/app/LoginView/index.jsx
+++ src/app/LoginView/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Modal from '../../components/Modal';
 import Header from '../../components/Header';
+import Input from '../../components/Input';
 
 const LoginView = () => (
   <>
     <Header />
-    <Modal header="sign in" />
+    <Modal header="sign in">
+      <Input label="User" value="yeah" />
+      <Input label="Password" value="yeah" />
+    </Modal>
   </>
 );
 
